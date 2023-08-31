2c8252b2bfbe5c471668d1efe8e05559a7176475
sopoour <sophia.auer@googlemail.com>
2023-08-31 14:57:28 +0200

    Layout and Header restyle

--- next.config.js
+++ next.config.js
@@ -4,6 +4,20 @@ const nextConfig = {
   compiler: {
     styledComponents: true,
   },
+  webpack(config, { buildId, isServer, webpack }) {
+    config.plugins.push(
+      new webpack.DefinePlugin({
+        // Define the build id so that it can be accessed in the client when reporting errors
+        'process.env.NEXT_BUILD_ID': JSON.stringify(buildId),
+        'process.env.NEXT_IS_SERVER': JSON.stringify(isServer),
+      })
+    );
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ['@svgr/webpack'],
+    });
+    return config;
+  },
 };
 
 module.exports = nextConfig;
