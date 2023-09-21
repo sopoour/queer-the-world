a4884aa6f51229c1e9c66899906df8906a635c64
sopoour <sophia.auer@googlemail.com>
2023-09-21 22:18:30 +0200

    adjusting pin styling and map

--- components/LeafletMap/index.tsx
+++ components/LeafletMap/index.tsx
@@ -2,9 +2,28 @@ import { FC } from 'react';
 import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet';
 import { styled } from 'styled-components';
 import ReactDOMServer from 'react-dom/server';
-import { FaLocationDot } from 'react-icons/fa6';
+import { MdLocationOn } from 'react-icons/md';
+import 'leaflet/dist/leaflet.css';
 import L from 'leaflet';
 import { Event } from '@prisma/client';
+
+const LocationIcon = styled(MdLocationOn)`
+  && {
+    width: 18px;
+    height: 18px;
+
+    path:last-of-type {
+      fill: #cafffc;
+      stroke: #06787f;
+      stroke-width: 1px;
+    }
+  }
+`;
+
+const Pin = L.divIcon({
+  html: ReactDOMServer.renderToString(<LocationIcon />),
+  className: '',
+});
 
 const StyledMapContainer = styled(MapContainer)`
   && {
@@ -13,36 +32,33 @@ const StyledMapContainer = styled(MapContainer)`
   }
 `;
 
-const StyledMarker = styled(Marker)`
-  > div {
-    border: red !important;
-  }
-`;
-
-const Pin = L.divIcon({
-  iconSize: [20, 20],
-  html: ReactDOMServer.renderToString(<FaLocationDot />),
-});
-
 type Props = {
   coordinates: [number, number];
   events: Event[];
 };
 
 const LeafletMap: FC<Props> = ({ coordinates, events }) => (
-  <MapContainer center={coordinates as [number, number]} zoom={13} scrollWheelZoom={false}>
+  <StyledMapContainer center={coordinates as [number, number]} zoom={2} scrollWheelZoom={false}>
     <TileLayer
       attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-      url="https://tiles.stadiamaps.com/tiles/osm_bright/{z}/{x}/{y}{r}.png"
+      url="https://tiles.stadiamaps.com/tiles/outdoors/{z}/{x}/{y}{r}.png"
     />
-    {/* {events.map((event) => (
-      <StyledMarker position={event.coordinates as [number, number]} icon={Pin} key={event.name}>
-        <Popup>
-          A pretty CSS3 popup. <br /> Easily customizable.
-        </Popup>
-      </StyledMarker>
-    ))} */}
-  </MapContainer>
+    {events.map((event) => {
+      if (event.coordinates?.length < 2) {
+        return null;
+      }
+      return (
+        <Marker
+          position={event.coordinates as [number, number]}
+          icon={Pin}
+          key={event.name}
+          riseOnHover
+        >
+          <Popup>{event.name}</Popup>
+        </Marker>
+      );
+    })}
+  </StyledMapContainer>
 );
 
 export default LeafletMap;
