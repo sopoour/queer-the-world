8a74b637aa39deb6f664ec515db16f3307bb9935
sopoour <36188360+sopoour@users.noreply.github.com>
2023-09-22 13:58:03 +0200

    Adding transitions between abstract and real map (#7)

--- components/LeafletMap/index.tsx
+++ components/LeafletMap/index.tsx
@@ -1,8 +1,9 @@
 import { FC } from 'react';
 import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet';
-import { styled } from 'styled-components';
+import { css, styled } from 'styled-components';
 import ReactDOMServer from 'react-dom/server';
 import { MdLocationOn } from 'react-icons/md';
+import { IoIosArrowRoundBack } from 'react-icons/io';
 import 'leaflet/dist/leaflet.css';
 import L from 'leaflet';
 import { Event } from '@prisma/client';
@@ -27,18 +28,43 @@ const Pin = L.divIcon({
 
 const StyledMapContainer = styled(MapContainer)`
   && {
+    width: 100%;
     height: 565px;
     border-radius: 10px !important;
   }
 `;
 
+const BackButton = styled.button`
+  display: flex;
+  align-items: center;
+  background-color: #cafffc;
+  border: 1px solid #06787f;
+  padding: 4px 8px;
+  border-radius: 6px;
+  margin-top: 8px;
+  margin-right: 8px;
+  pointer-events: all !important;
+  color: #06787f;
+  font-weight: 600;
+  font-size: 12px;
+  &:hover {
+    filter: brightness(0.95);
+  }
+
+  svg {
+    width: 20px;
+    height: 15px;
+  }
+`;
+
 type Props = {
-  coordinates: [number, number];
-  events: Event[];
+  coordinates?: [number, number];
+  events?: Event[];
   continentName: string;
+  onHideMap: () => void;
 };
 
-const LeafletMap: FC<Props> = ({ coordinates, events, continentName }) => (
+const LeafletMap: FC<Props> = ({ coordinates, events, continentName, onHideMap }) => (
   <StyledMapContainer
     center={coordinates as [number, number]}
     zoom={2.5}
@@ -49,7 +75,10 @@ const LeafletMap: FC<Props> = ({ coordinates, events, continentName }) => (
       attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
       url="https://tiles.stadiamaps.com/tiles/outdoors/{z}/{x}/{y}{r}.png"
     />
-    {events.map((event) => {
+    <BackButton className="leaflet-top leaflet-right" onClick={onHideMap}>
+      <IoIosArrowRoundBack /> Go Back
+    </BackButton>
+    {events?.map((event) => {
       if (event.coordinates?.length < 2) {
         return null;
       }
